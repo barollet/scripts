8012aea38a5652c633a07ed25c2d3be538d049bb
Theo Barollet <theo.barollet@ensimag.fr>
2020-02-18 19:20:02 +0100

    reset lpt watcher bot to printing to stdout

--- lpt_watcher/src/main.rs
+++ lpt_watcher/src/main.rs
@@ -1,4 +1,4 @@
-extern crate web3;
+#![feature(async_closure)]
 
 mod initialization;
 mod round_detector;
@@ -8,6 +8,8 @@ use std::sync::atomic::Ordering;
 
 use web3::futures::{Future, Stream};
 use web3::types::U256;
+
+use chrono::prelude::Utc;
 
 use initialization::*;
 use round_detector::RoundDetector;
@@ -20,15 +22,15 @@ async fn main() {
 
     // Subscribing to reward() transactions
     let mut reward_subscription = init.reward_call_subscription();
-    println!("Subscribed to reward() call");
+    eprintln!("Subscribed to reward() call");
 
     // Subscribing to new block header
     let mut new_block_subscription = init.new_block_subscription();
-    println!("Subscribed to new block headers");
+    eprintln!("Subscribed to new block headers");
 
     // Round manager contract interface
     let contract_interface = init.contract_interface();
-    println!("Round manager contract interface instanciated");
+    eprintln!("Round manager contract interface instanciated");
 
     // Shared value tracking if the reward call has been done
     let current_round_transaction_done = init.transaction_state();
@@ -38,13 +40,13 @@ async fn main() {
 
     // Initializing transaction detector
     let transaction_detector = TransactionDetector::new();
-    println!("Transaction detector initialized");
+    eprintln!("Transaction detector initialized");
 
     // Initializing round detector
     let mut round_detector = RoundDetector::from_contract(contract_interface, safety_window);
-    println!("Round detector initialized");
+    eprintln!("Round detector initialized");
 
-    println!("Running...");
+    eprintln!("Running...");
 
     // Watching Livepeer rounds
     let round_detector_stream = (&mut new_block_subscription).for_each(|block_header| {
@@ -59,7 +61,7 @@ async fn main() {
         } else if round_detector.reached_security_window(block_number) {
             // Checks that the transaction has be done, otherwise triggers an alert
             if !current_round_transaction_done.load(Ordering::Acquire) {
-                // Triggers an alert
+                // Triggers an alert on standard output
                 println!("Transaction has to be done");
             }
         }
@@ -67,12 +69,26 @@ async fn main() {
     });
 
     let reward_stream = (&mut reward_subscription).for_each(|log| {
-        // if the transaction is a success
-        if transaction_detector.has_valid_transaction_been_made(init.web3(), log) {
-            println!("Transaction success");
-            // sets the transaction as done
-            current_round_transaction_done.store(true, Ordering::Release);
-        }
+        transaction_detector
+            .exctract_transaction_from_log(init.web3(), log)
+            .map(|transaction| {
+                // prints the transaction on standard output
+                let url = format!("https://etherscan.io/tx/{}", transaction.hash);
+                let value = transaction.value.to_string();
+                println!(
+                    "Received new transaction:\nTime: {} UTC\nToken transfered: {}\nUrl: {}\n",
+                    Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+                    value,
+                    url
+                );
+
+                // If the transaction is a success
+                if transaction.value != U256::zero() {
+                    eprintln!("Transaction success");
+                    // sets the transaction as done
+                    current_round_transaction_done.store(true, Ordering::Release);
+                }
+            });
 
         Ok(())
     });
---
+++ lpt_watcher/src/transaction_detector.rs
@@ -0,0 +1,23 @@
+use web3::futures::Future;
+use web3::types::{Log, Transaction, TransactionId};
+
+use crate::initialization::Web3Itf;
+
+pub struct TransactionDetector {}
+
+impl TransactionDetector {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn exctract_transaction_from_log(&self, web3: &Web3Itf, log: Log) -> Option<Transaction> {
+        // If there is no transaction hash we do as if the transaction was not done
+        log.transaction_hash.and_then(|tx_hash| {
+            web3.eth()
+                .transaction(TransactionId::Hash(tx_hash))
+                .wait()
+                .ok()
+                .unwrap()
+        })
+    }
+}
